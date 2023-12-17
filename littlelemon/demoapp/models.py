c385649ff86e73dbda8c733e461c4697ce5f5e5e
NareshSomu <namershsomu27@gmail.com>
2023-12-17 15:47:59 +0530

    changed

--- littlelemon/demoapp/models.py
+++ littlelemon/demoapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Booking(models.Model):
+    first_name=models.CharFeild(max_length=200)
+    last_name=models.CharFeild(max_length=200)
+    guest_count=models.IntegerField()
+    reservation_time=models.DateField(auto_now=True)
+    comments=models.CharField(max_length=1000)
+
 # Create your models here.
