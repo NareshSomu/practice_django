e350f6b45f5db4629a1a3aa9928bb5ff33545d54
NareshSomu <namershsomu27@gmail.com>
2023-12-17 16:27:50 +0530

    creating form

--- littlelemon/demoapp/models.py
+++ littlelemon/demoapp/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Booking(models.Model):
-    first_name=models.CharFeild(max_length=200)
-    last_name=models.CharFeild(max_length=200)
+    first_name=models.CharField(max_length=200)
+    last_name=models.CharField(max_length=200)
     guest_count=models.IntegerField()
-    reservation_time=models.DateField(auto_now=True)
+    reservation_tine=models.DateTimeField(auto_now=True)
     comments=models.CharField(max_length=1000)
 
 # Create your models here.
--- littlelemon/demoapp/views.py
+++ littlelemon/demoapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import BookingForm
+
+def form_view(request):
+    form=BookingForm()
+    if request.method=='POST':
+        form=BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context={'form':form}
+    return render(request,'booking.html',context)
 
 # Create your views here.
